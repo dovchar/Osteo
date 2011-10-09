b44d6d7364af526401e9b68e43cececad03f6bd9
Tanguy Krotoff <tkrotoff@gmail.com>
2011-10-09 23:31:47 +0200

    Create Osteo using Ruby on Rails 3.1.1, this is the original structure generated by 'rails new Osteo' command line

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osteo::Application.config.secret_token = 'fc0cdb2d9ee328661e1d7fa5d4f87146bea62cc18fef8f2d1b414e95033ee1007b6cc175c1174b42f597e3e65933db1b0396ae9403c6f642bd69518982143835'
