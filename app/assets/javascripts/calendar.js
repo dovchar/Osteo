993072a731b29a400e0805f51c1ddabf889624bb
Tanguy Krotoff <tkrotoff@gmail.com>
2011-10-10 22:37:05 +0200

    Test jQuery and jQuery UI

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,2 +1,4 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $("#calendar").html("Hello World from jQuery!");
+  $("#date").datepicker();
+});
