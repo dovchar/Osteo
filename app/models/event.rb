00cc5bc4466d109b109be6f9ad2bd3bf1063b65b
Tanguy Krotoff <tkrotoff@gmail.com>
2011-10-19 21:36:15 +0200

    Unit tests for Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ActiveRecord::Base
 
+  validates :title, :presence => true
+  validates :starts_at, :presence => true
+  validates :ends_at, :presence => true
+  validate :validate_ends_at_after_starts_at
+
   # Need to override the JSON view to return what FullCalendar is expecting.
   # See http://arshaw.com/fullcalendar/docs/event_data/Event_Object/
   def as_json(options = nil)
@@ -12,5 +17,13 @@ class Event < ActiveRecord::Base
       allDay: all_day,
       url: Rails.application.routes.url_helpers.event_path(id)
     }
+  end
+
+  private
+
+  def validate_ends_at_after_starts_at
+    if starts_at && ends_at
+      errors.add(:ends_at, "can't be before the starting date") if ends_at < starts_at
+    end
   end
 end
--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "event attributes must not be empty" do
+    event = Event.new
+    assert event.invalid?
+    assert event.errors[:title].any?
+    assert event.errors[:starts_at].any?
+    assert event.errors[:ends_at].any?
+
+    assert !event.save
+    assert_equal "can't be blank", event.errors[:title].join('; ')
+  end
+
+  test "event end date must be after start date" do
+    event = Event.new(title: 'Hello', starts_at: 1, ends_at: 0)
+    assert event.invalid?
+    assert_equal "can't be before the starting date", event.errors[:ends_at].join('; ')
+
+    event = Event.new(title: 'Hello', starts_at: 0, ends_at: 0)
+    assert event.valid?
+
+    event = Event.new(title: 'Hello', starts_at: 0, ends_at: 1)
+    assert event.valid?
+  end
+
 end
