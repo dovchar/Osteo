e701f03ca6343f33e00e87ee61616a857fd13d4e
Tanguy Krotoff <tkrotoff@gmail.com>
2011-10-24 02:28:12 +0200

    Trying to test CalendarController Don't know how to do it, see http://stackoverflow.com/questions/7866401/functional-tests-jquery-and-assert-select-jquery

--- test/functional/calendar_controller_test.rb
+++ test/functional/calendar_controller_test.rb
@@ -4,6 +4,22 @@ class CalendarControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+
+    #assert_select_jquery :html, '#calendar' do
+    #  assert_select '.fc-header', count: 1
+    #  assert_select '.fc-content', count: 1
+    #end
+  end
+
+  test "should get index and move to specified date" do
+    get :index, { date: events(:alisson).starts_at }
+    assert_response :success
+
+    #assert_select_jquery :html, '#calendar' do
+    #  assert_select '.fc-header-title h2', 'October 2011'
+    #  assert_select '.fc-event-time', '12a'
+    #  assert_select '.fc-event-title', 'Appointment with Alisson'
+    #end
   end
 
 end
