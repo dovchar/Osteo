4ea2b1dc03e117f554f79c84fe1e3e38d1fff4e1
Tanguy Krotoff <tkrotoff@gmail.com>
2011-11-08 20:14:47 +0100

    Improve new event form

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,33 +10,38 @@ class Time
   def floor(seconds = 60)
     Time.at((to_f / seconds).floor * seconds)
   end
-
-  def to_html_format
-    strftime('%m/%d/%Y %R')
-  end
 end
 
 class ActionView::Helpers::FormBuilder
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::JavaScriptHelper
 
-  def my_datetime_select(method, options = {})
-    options['value'] = Time.now.round(10.minutes).to_html_format
-    options['size'] = 18
+  def jquery_datetime_select(method, time, date_options = {}, time_options = {})
+    date_options['value'] = time.strftime('%d/%m/%Y')
+    date_options['size'] = 8
+    date_options['index'] = 'date'
+    date_field = @template.text_field(@object_name, method, date_options)
 
-    datetime_field = @template.text_field(@object_name, method, options)
+    time_options['value'] = time.round(10.minutes).strftime('%R')
+    time_options['size'] = 3
+    time_options['index'] = 'time'
+    time_field = @template.text_field(@object_name, method, time_options)
 
-    datetime_picker = javascript_tag "
+    javascript = javascript_tag "
       $(document).ready(function() {
-        $('##{tag_id(method)}').datetimepicker({
-          stepMinute: 10,
-          showButtonPanel: false,
+
+        $('##{tag_id_with_index(method, date_options['index'])}').datepicker({
           dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
           dateFormat: 'dd/mm/yy'
         });
+
+        $('##{tag_id_with_index(method, time_options['index'])}').timepicker({
+          stepMinute: 10,
+          showButtonPanel: false,
+        });
       });"
 
-    return datetime_field + datetime_picker
+    return date_field + time_field + javascript
   end
 
   private
@@ -44,6 +49,11 @@ class ActionView::Helpers::FormBuilder
   # Copy-pasted from ActionView::Helpers::InstanceTag
   def tag_id(method)
     "#{sanitized_object_name}_#{sanitized_method_name(method)}"
+  end
+
+  # Copy-pasted from ActionView::Helpers::InstanceTag
+  def tag_id_with_index(method, index)
+    "#{sanitized_object_name}_#{index}_#{sanitized_method_name(method)}"
   end
 
   # Copy-pasted from ActionView::Helpers::InstanceTag
