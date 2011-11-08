a76cd13cbd4c61d88f41a78401f2f39b765cf6fb
Tanguy Krotoff <tkrotoff@gmail.com>
2011-11-08 10:03:42 +0100

    Improve new event form

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,17 +1,28 @@
 module EventsHelper
 end
 
+# FIXME Should be moved somewhere else
+class Time
+  def round(seconds = 60)
+    Time.at((to_f / seconds).round * seconds)
+  end
+
+  def floor(seconds = 60)
+    Time.at((to_f / seconds).floor * seconds)
+  end
+
+  def to_html_format
+    strftime('%m/%d/%Y %R')
+  end
+end
+
 class ActionView::Helpers::FormBuilder
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::JavaScriptHelper
 
   def my_datetime_select(method, options = {})
-    options['value'] = Time.now
+    options['value'] = Time.now.round(10.minutes).to_html_format
     options['size'] = 18
-
-    puts method
-    puts @object_name
-    puts tag_id(method)
 
     datetime_field = @template.text_field(@object_name, method, options)
 
@@ -20,7 +31,8 @@ class ActionView::Helpers::FormBuilder
         $('##{tag_id(method)}').datetimepicker({
           stepMinute: 10,
           showButtonPanel: false,
-          dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+          dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+          dateFormat: 'dd/mm/yy'
         });
       });"
 
