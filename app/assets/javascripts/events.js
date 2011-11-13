223cf0ea41707b34cd5ea909e5efb57251cbdd7d
Tanguy Krotoff <tkrotoff@gmail.com>
2011-11-13 20:53:27 +0100

    Add jquery-ui-addresspicker

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,3 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-//= require jquery-ui-timepicker-addon/jquery-ui-timepicker-addon.js
+//= require jquery-ui-timepicker/jquery-ui-timepicker-addon.js
