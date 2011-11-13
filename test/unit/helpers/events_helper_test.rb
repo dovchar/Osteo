895fa113ea4b663869d48f7508b24ef79a331525
Tanguy Krotoff <tkrotoff@gmail.com>
2011-11-13 15:02:52 +0100

    Tests for Time#round

--- test/unit/helpers/events_helper_test.rb
+++ test/unit/helpers/events_helper_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class EventsHelperTest < ActionView::TestCase
+  test "time should round" do
+    time = Time.new(2011, 11, 13, 03, 50, 00).round(10.minutes)
+    assert_equal time, Time.new(2011, 11, 13, 03, 50, 00)
+
+    time = Time.new(2011, 11, 13, 03, 51, 00).round(10.minutes)
+    assert_equal time, Time.new(2011, 11, 13, 03, 50, 00)
+
+    time = Time.new(2011, 11, 13, 03, 59, 00).round(10.minutes)
+    assert_equal time, Time.new(2011, 11, 13, 04, 00, 00)
+
+    time = Time.new(2011, 11, 13, 13, 42, 24).round()
+    assert_equal time, Time.new(2011, 11, 13, 13, 42, 00)
+
+    time = Time.new(2011, 11, 13, 13, 42, 24).round(1.hour)
+    assert_equal time, Time.new(2011, 11, 13, 14, 00, 00)
+  end
 end
