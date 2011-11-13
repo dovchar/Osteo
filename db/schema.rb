fd9fdba72b35a4972b26dc6debaeec5b9b4a8c41
Tanguy Krotoff <tkrotoff@gmail.com>
2011-11-13 20:22:27 +0100

    rails generate migration add_location_to_event location:string rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111012231537) do
+ActiveRecord::Schema.define(:version => 20111113190753) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20111012231537) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location"
   end
 
 end
