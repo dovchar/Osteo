722c6c1e78be65e8560350bd39743ecc084e8aa3
Tanguy Krotoff <tkrotoff@gmail.com>
2011-11-14 01:17:39 +0100

    Add jquery-ui-addresspicker

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //= require jquery-ui-timepicker/jquery-ui-timepicker-addon.js
+//= require jquery-ui-addresspicker/jquery.ui.addresspicker.js
+
+$(document).ready(function() {
+  var addresspickerMap = $("#event_location").addresspicker({
+    elements: {
+      map: "#map"
+    }
+  });
+  var gmarker = addresspickerMap.addresspicker("marker");
+  gmarker.setVisible(true);
+  addresspickerMap.addresspicker("updatePosition");
+});
