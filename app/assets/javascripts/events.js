de456560986cf2233976d262e4c418ffb9372d94
Tanguy Krotoff <tkrotoff@gmail.com>
2011-12-05 22:17:55 +0100

    jQuery UI addresspicker fails nicely if google.maps API is unreachable

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,12 +2,26 @@
 //= require jquery-ui-addresspicker/jquery.ui.addresspicker.js
 
 $(document).ready(function() {
-  var addresspickerMap = $("#event_location").addresspicker({
-    elements: {
-      map: "#map"
-    }
-  });
-  var gmarker = addresspickerMap.addresspicker("marker");
-  gmarker.setVisible(true);
-  addresspickerMap.addresspicker("updatePosition");
+  try {
+
+    var addresspickerMap = $("#event_location").addresspicker({
+      mapOptions: {
+        center: new google.maps.LatLng(
+          //Default location is Paris
+          48.856614,
+          2.3522219000000177
+        )
+      },
+      elements: {
+        map: "#map"
+      },
+      draggableMarker: false
+    });
+    var gmarker = addresspickerMap.addresspicker("marker");
+    gmarker.setVisible(true);
+    addresspickerMap.addresspicker("updatePosition");
+
+  } catch (error) {
+    //This happens when google.maps API cannot be reached
+  }
 });
