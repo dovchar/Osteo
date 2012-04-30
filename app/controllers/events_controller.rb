b2d3b49b3230201915e39756c5d6306db92addcb
Tanguy Krotoff <tkrotoff@gmail.com>
2012-04-30 17:09:19 +0200

    Fix new_tooltip.html.erb edit link

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -55,16 +55,9 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        if params[:edit_event]
-          # Edit button was clicked
-          #format.js { render action: 'edit' }
-          format.js { redirect_to edit_event_path }
-        else
-          # Regular create action
-          format.html { redirect_to calendar_url, notice: 'Event was successfully created.' }
-          format.json { render json: @event, status: :created, location: @event }
-          format.js # create.js.erb
-        end
+        format.html { redirect_to calendar_url, notice: 'Event was successfully created.' }
+        format.json { render json: @event, status: :created, location: @event }
+        format.js # create.js.erb
       else
         format.html { render action: 'new' }
         format.json { render json: @event.errors, status: :unprocessable_entity }
