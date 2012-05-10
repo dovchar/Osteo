737692fbc7cb15b981d46a4ff1803024577f6ac4
Tanguy Krotoff <tkrotoff@gmail.com>
2012-05-10 22:08:20 +0200

    Switch to a mountable engine See http://guides.rubyonrails.org/engines.html See http://railscasts.com/episodes/277-mountable-engines

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'e1cfcac161f30bb508afbe9abfee7501a300755aa434ffdc72e5307bc0b96a2c34b9c10660eb9dc21ebb9af38f296f875675914753d9668d12127ef54573009d'
