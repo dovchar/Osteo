d996899f31588557a628aca00a2c524b89b4a141
Tanguy Krotoff <tkrotoff@gmail.com>
2012-06-08 19:59:20 +0200

    Switch to jquery-ui-rails gem

--- app/assets/javascripts/calendar/events.js
+++ app/assets/javascripts/calendar/events.js
@@ -1,3 +1,4 @@
+//= require jquery.ui.autocomplete
 //= require jquery-ui-addresspicker/jquery.ui.addresspicker.js
 // require bootstrap-datepicker
 //= require jquery-timepicker/jquery.timepicker.js
