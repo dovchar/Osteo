c3fcfdcc540b2f8d3977ff248853a51618c24e61
Tanguy Krotoff <tkrotoff@gmail.com>
2012-06-26 16:33:30 +0200

    Refactoring

--- app/assets/javascripts/calendar/events.js
+++ app/assets/javascripts/calendar/events.js
@@ -6,4 +6,18 @@
 
 $(document).ready(function() {
   $('#event_color').simplecolorpicker();
+
+  var all_day = $('#event_all_day');
+  all_day.change(function() {
+    var starts_at_time = $('#event_starts_at_time');
+    var ends_at_time = $('#event_ends_at_time');
+    if (all_day.prop('checked')) {
+      starts_at_time.hide();
+      ends_at_time.hide();
+    } else {
+      starts_at_time.show();
+      ends_at_time.show();
+    }
+  });
+  all_day.change();
 });
